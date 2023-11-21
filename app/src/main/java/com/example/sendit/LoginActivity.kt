b64bacd52ecf7c50cc6a024cbbe6926cacc2c697
Andrii Drobitko <anddro458@gmail.com>
2023-11-21 10:46:56 +0100

    testcommit1

--- app/src/main/java/com/example/sendit/LoginActivity.kt
+++ app/src/main/java/com/example/sendit/LoginActivity.kt
@@ -1,11 +1,16 @@
 package com.example.sendit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+//        val gso: GoogleSignInOptions = Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestEmail()
+//            .build()
+//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 }
