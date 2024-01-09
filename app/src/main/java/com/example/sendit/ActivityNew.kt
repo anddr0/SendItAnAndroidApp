092c7ad8e5c1ed5e0c3812cac701e062b848dfb9
Jannekk <janis.bo.01@gmail.com>
2024-01-09 09:29:19 +0100

    test

--- app/src/main/java/com/example/sendit/ActivityNew.kt
+++
@@ -1,4 +0,0 @@
-package com.example.sendit
-
-class ActivityNew {
-}
--- app/src/main/java/com/example/sendit/LoginActivity.kt
+++ app/src/main/java/com/example/sendit/LoginActivity.kt
@@ -44,7 +44,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signIn() {
-        val sing_in_intent: Intent = gsc.getSignInIntent()
+        val sing_in_intent: Intent = gsc.signInIntent
         startActivityForResult(sing_in_intent, 1000)
     }
 
@@ -57,7 +57,8 @@ class LoginActivity : AppCompatActivity() {
                 task.getResult(ApiException::class.java)
                 navigateToSecondActivity()
             } catch (e: ApiException) {
-                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT)
+
+                Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT)
                     .show()
             }
         }
